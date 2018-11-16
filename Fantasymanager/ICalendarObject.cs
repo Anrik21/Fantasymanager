faad020fb981a766a366e99b07a70b769e1624a4
Anton Nilsson <anrik21@gmail.com>
2018-11-16 13:19:46 +0100

    added a first version of interface

---
+++ Fantasymanager/ICalendarObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasymanager
+{
+    public struct CustomDates
+    {
+        public int y, m, d;
+
+        public CustomDates(int year, int month, int day)
+        {
+            y = year;
+            m = month;
+            d = day;
+        }
+    }
+
+    interface ICalendarObject<T>
+    {
+        CustomDates GetDates { get; set; }
+        List<T> CollectionOfDates { get; set;}
+        string DateName { get; set; }
+        List<T> CollectionOfEvents { get; set; }
+    }
+}
