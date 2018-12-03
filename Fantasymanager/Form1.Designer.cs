2881a68b3d08f07addf2918904a0c5e6b1a6c8fb
Anton Nilsson <anrik21@gmail.com>
2018-12-03 09:39:12 +0100

    Commit so I can work on it on laptop as well.

--- Fantasymanager/Form1.Designer.cs
+++ Fantasymanager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fantasymanager
 {
-    partial class Form1
+    partial class Dungeonportal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 162);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generator";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 162);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Click me!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(330, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 162);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "But not implemented :(";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Dungeonportal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(501, 191);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Dungeonportal";
+            this.Text = "Fantasymanager - Portal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Fantasymanager/Form1.cs
+++ Fantasymanager/Form1.cs
@@ -10,11 +10,33 @@ using System.Windows.Forms;
 
 namespace Fantasymanager
 {
-    public partial class Form1 : Form
+    public partial class Dungeonportal : Form
     {
-        public Form1()
+        Random random;
+
+        public Dungeonportal()
         {
             InitializeComponent();
+            random = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int currentSound = random.Next(1, 4);
+
+            if (currentSound == 1)
+                System.Media.SystemSounds.Beep.Play();
+            if (currentSound == 2)
+                System.Media.SystemSounds.Asterisk.Play();
+            if (currentSound == 3)
+                System.Media.SystemSounds.Exclamation.Play();
+            if (currentSound == 4)
+                System.Media.SystemSounds.Hand.Play();
         }
     }
 }
